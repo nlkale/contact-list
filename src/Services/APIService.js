a0c20ea0ed4a0f1f34ea88f3dad3348873fc626f
Mykola Kosynets <kolyan.kosenez@gmail.com>
2021-10-17 13:06:21 +0300

    added redux and spinner

--- src/Services/APIService.js
+++ src/Services/APIService.js
@@ -1,33 +1,33 @@
-class ApiService{
-   URL = "https://contact-list-b089e-default-rtdb.firebaseio.com/list.json";
+class ApiService {
+  URL = "https://contact-list-b089e-default-rtdb.firebaseio.com/list.json";
 
-   async GetContactList(){
-      const List = await fetch(this.URL)
+  async GetContactList() {
+    const List = await fetch(this.URL)
       .then(responce => {
-         return responce.json();
+        return responce.json();
       })
       .then(data => {
-         if(data == null){
-            return []
-         } else{
-            return data
-         }
-
+        if (data == null) {
+          return [];
+        } else {
+          return data;
+        }
       })
       .catch(err => console.log(err));
-      return List;
-   }
+    return List;
+  }
 
-   async UpdateDatabase(List){
-      const result = await fetch(this.URL, {
-         headers:{
-            "Content-Type": "aplication/json"
-         },
-         method: "PUT",
-         body: JSON.stringify(List)
-      });
-      return result;
-   }
+  async UpdateDatabase(List) {
+    const result = await fetch(this.URL, {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "PUT",
+      body: JSON.stringify(List)
+    });
+    return result;
+  }
 }
+
 const API = new ApiService();
 export default API;
